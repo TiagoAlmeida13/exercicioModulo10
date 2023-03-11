2fcbdce908fe7642ec4239a54e2061db493201c0
TiagoAlmeida13 <tyygo@live.com>
2023-03-11 10:37:59 -0300

    consulta corrigida e funcionando.

--- src/components/formFipe/formFipe.js
+++ src/components/formFipe/formFipe.js
@@ -1,13 +1,16 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
+import './formFipe.css'
+
 function MarcasDropdown() {
     const [marcas, setMarcas] = useState([]);
     const [marcaSelecionada, setMarcaSelecionada] = useState('');
     const [modelos, setModelos] = useState([]);
     const [modeloSelecionado, setModeloSelecionado] = useState('');
     const [anos, setAnos] = useState([]);
-    const [details, setDetails] = useState([]);
+    const [anoSelecionado, setAnoSelecionado] = useState('');
+    const [details, setDetails] = useState({});
 
     useEffect(() => {
         const fetchMarcas = async () => {
@@ -21,6 +24,9 @@ function MarcasDropdown() {
         const fetchModelos = async () => {
             const response = await axios.get(`https://parallelum.com.br/fipe/api/v1/carros/marcas/${marcaSelecionada}/modelos`);
             setModelos(response.data.modelos);
+            setAnos([]);
+            setAnoSelecionado('');
+            setDetails({});
         };
         if (marcaSelecionada !== '') {
             fetchModelos();
@@ -31,7 +37,8 @@ function MarcasDropdown() {
         const fetchAnos = async () => {
             const response = await axios.get(`https://parallelum.com.br/fipe/api/v1/carros/marcas/${marcaSelecionada}/modelos/${modeloSelecionado}/anos`);
             setAnos(response.data);
-
+            setAnoSelecionado('');
+            setDetails({});
         };
         if (modeloSelecionado !== '') {
             fetchAnos();
@@ -40,18 +47,17 @@ function MarcasDropdown() {
 
     useEffect(() => {
         const fetchDetails = async () => {
-            const response = await axios.get(`https://parallelum.com.br/fipe/api/v1/carros/marcas/${marcaSelecionada}/modelos/${modeloSelecionado}/anos/${details}`);
+            const response = await axios.get(`https://parallelum.com.br/fipe/api/v1/carros/marcas/${marcaSelecionada}/modelos/${modeloSelecionado}/anos/${anoSelecionado}`);
             setDetails(response.data);
         };
-        if (anos !== '') {
+        if (anoSelecionado !== '') {
             fetchDetails();
         }
-    }, [marcaSelecionada, modeloSelecionado, anos]);
+    }, [marcaSelecionada, modeloSelecionado, anoSelecionado]);
 
     const handleMarcaChange = (event) => {
         setMarcaSelecionada(event.target.value);
         setModeloSelecionado('');
-        setAnos([]);
     };
 
     const handleModeloChange = (event) => {
@@ -59,62 +65,67 @@ function MarcasDropdown() {
     };
 
     const handleAnoChange = (event) => {
-        setDetails(event.target.value);
+        setAnoSelecionado(event.target.value);
     };
 
     return (
-        <div>
-            <label htmlFor="marcas">Selecione uma marca:</label>
-            <select id="marcas" name="marcas" onChange={handleMarcaChange}>
-                <option value="">Selecione...</option>
-                {marcas.map(marca => (
-                    <option key={marca.codigo} value={marca.codigo}>{marca.nome}</option>
-                ))}
-            </select>
-            {modelos.length > 0 && (
-                <div>
-                    <label htmlFor="modelos">Selecione um modelo:</label>
-                    <select id="modelos" name="modelos" onChange={handleModeloChange}>
-                        <option value="">Selecione...</option>
-                        {modelos.map(modelo => (<option key={modelo.codigo} value={modelo.codigo}>{modelo.nome}</option>
-                        ))}
-                    </select>
-                </div>
-            )}
-            {anos.length > 0 && (
-                <div>
-                    <label htmlFor="anos">Selecione o ano:</label>
-                    <select id="anos" name="anos" onChange={handleAnoChange}>
-                        <option value="">Selecione...</option>
-                        {anos.map(ano => (<option key={ano.codigo} value={ano.codigo}>{ano.nome}</option>
-                        ))}
-                    </select>
-                </div>
-            )}
+        <div className='search'>
+            <div>
+                <label htmlFor="marcas">Selecione uma marca:</label>
+                <select id="marcas" name="marcas" onChange={handleMarcaChange}>
+                    <option value="">Selecione...</option>
+                    {marcas.map(marca => (
+                        <option key={marca.codigo} value={marca.codigo}>{marca.nome}</option>
+                    ))}
+                </select>
+                {modelos.length > 0 && (
+                    <div>
+                        <label htmlFor="modelos">Selecione um modelo:</label>
+                        <select id="modelos" name="modelos" onChange={handleModeloChange}>
+                            <option value="">Selecione...</option>
+                            {modelos.map(modelo => (<option key={modelo.codigo} value={modelo.codigo}>{modelo.nome}</option>
+                            ))}
+                        </select>
+                    </div>
+                )}
+                {anos.length > 0 && (
+                    <div>
+                        <label htmlFor="anos">Selecione o ano:</label>
+                        <select id="anos" name="anos" onChange={handleAnoChange}>
+                            <option value="">Selecione...</option>
+                            {anos.map(ano => (<option key={ano.codigo} value={ano.codigo}>{ano.nome}</option>
+                            ))}
+                        </select>
+                    </div>
+                )}
+            </div>
 
-            {details.length > 0 && (
-                <div>
-                    <h2>Detalhes</h2>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Marca</th>
-                                <th>Modelo</th>
-                                <th>Ano</th>
-                                <th>Preço</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>{details.Marca}</td>
-                                <td>{details.Modelo}</td>
-                                <td>{details.AnoModelo}</td>
-                                <td>{details.Valor}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            )}
+            <div>
+                {anoSelecionado.length > 0 && (
+                    <div>
+                        <h2>Detalhes</h2>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Marca</th>
+                                    <th>Modelo</th>
+                                    <th>Ano</th>
+                                    <th>Preço</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{details.Marca}</td>
+                                    <td>{details.Modelo}</td>
+                                    <td>{details.AnoModelo}</td>
+                                    <td>{details.Valor}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                )}
+            </div>
+
 
         </div>
     );
