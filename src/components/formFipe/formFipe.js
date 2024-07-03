f1c275c1cc0b876aa0b6bc06a66e554e4b3ede15
TiagoAlmeida13 <tyygo@live.com>
2024-07-02 21:14:51 -0300

    update

--- src/components/formFipe/formFipe.js
+++ src/components/formFipe/formFipe.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-
-import './formFipe.css'
+import './formFipe.css';
 
 function MarcasDropdown() {
     const [marcas, setMarcas] = useState([]);
@@ -12,52 +11,58 @@ function MarcasDropdown() {
     const [anoSelecionado, setAnoSelecionado] = useState('');
     const [details, setDetails] = useState({});
 
+    const fetchMarcas = async () => {
+        const response = await axios.get('https://parallelum.com.br/fipe/api/v1/carros/marcas');
+        setMarcas(response.data);
+    };
+
+    const fetchModelos = async (marcaCodigo) => {
+        const response = await axios.get(`https://parallelum.com.br/fipe/api/v1/carros/marcas/${marcaCodigo}/modelos`);
+        setModelos(response.data.modelos);
+    };
+
+    const fetchAnos = async (marcaCodigo, modeloCodigo) => {
+        const response = await axios.get(`https://parallelum.com.br/fipe/api/v1/carros/marcas/${marcaCodigo}/modelos/${modeloCodigo}/anos`);
+        setAnos(response.data);
+    };
+
+    const fetchDetails = async (marcaCodigo, modeloCodigo, anoCodigo) => {
+        const response = await axios.get(`https://parallelum.com.br/fipe/api/v1/carros/marcas/${marcaCodigo}/modelos/${modeloCodigo}/anos/${anoCodigo}`);
+        setDetails(response.data);
+    };
+
     useEffect(() => {
-        const fetchMarcas = async () => {
-            const response = await axios.get('https://parallelum.com.br/fipe/api/v1/carros/marcas');
-            setMarcas(response.data);
-        };
         fetchMarcas();
     }, []);
 
     useEffect(() => {
-        const fetchModelos = async () => {
-            const response = await axios.get(`https://parallelum.com.br/fipe/api/v1/carros/marcas/${marcaSelecionada}/modelos`);
-            setModelos(response.data.modelos);
+        if (marcaSelecionada) {
+            fetchModelos(marcaSelecionada);
+            setModeloSelecionado('');
+            setModelos([]);
             setAnos([]);
             setAnoSelecionado('');
             setDetails({});
-        };
-        if (marcaSelecionada !== '') {
-            fetchModelos();
         }
     }, [marcaSelecionada]);
 
     useEffect(() => {
-        const fetchAnos = async () => {
-            const response = await axios.get(`https://parallelum.com.br/fipe/api/v1/carros/marcas/${marcaSelecionada}/modelos/${modeloSelecionado}/anos`);
-            setAnos(response.data);
+        if (modeloSelecionado) {
+            fetchAnos(marcaSelecionada, modeloSelecionado);
             setAnoSelecionado('');
+            setAnos([]);
             setDetails({});
-        };
-        if (modeloSelecionado !== '') {
-            fetchAnos();
         }
-    }, [marcaSelecionada, modeloSelecionado]);
+    }, [modeloSelecionado, marcaSelecionada]);
 
     useEffect(() => {
-        const fetchDetails = async () => {
-            const response = await axios.get(`https://parallelum.com.br/fipe/api/v1/carros/marcas/${marcaSelecionada}/modelos/${modeloSelecionado}/anos/${anoSelecionado}`);
-            setDetails(response.data);
-        };
-        if (anoSelecionado !== '') {
-            fetchDetails();
+        if (anoSelecionado) {
+            fetchDetails(marcaSelecionada, modeloSelecionado, anoSelecionado);
         }
-    }, [marcaSelecionada, modeloSelecionado, anoSelecionado]);
+    }, [anoSelecionado, marcaSelecionada, modeloSelecionado]);
 
     const handleMarcaChange = (event) => {
         setMarcaSelecionada(event.target.value);
-        setModeloSelecionado('');
     };
 
     const handleModeloChange = (event) => {
@@ -73,7 +78,7 @@ function MarcasDropdown() {
             <div className='formSearch'>
                 <h2>Buscar veículo</h2>
                 <label htmlFor="marcas">Selecione uma marca:</label>
-                <select id="marcas" name="marcas" onChange={handleMarcaChange}>
+                <select id="marcas" name="marcas" onChange={handleMarcaChange} value={marcaSelecionada}>
                     <option value="">Selecione...</option>
                     {marcas.map(marca => (
                         <option key={marca.codigo} value={marca.codigo}>{marca.nome}</option>
@@ -82,27 +87,25 @@ function MarcasDropdown() {
                 {modelos.length > 0 && (
                     <div>
                         <label htmlFor="modelos">Selecione um modelo:</label>
-                        <select id="modelos" name="modelos" onChange={handleModeloChange}>
+                        <select id="modelos" name="modelos" onChange={handleModeloChange} value={modeloSelecionado}>
                             <option value="">Selecione...</option>
-                            {modelos.map(modelo => (<option key={modelo.codigo} value={modelo.codigo}>{modelo.nome}</option>
-                            ))}
+                            {modelos.map(modelo => (<option key={modelo.codigo} value={modelo.codigo}>{modelo.nome}</option>))}
                         </select>
                     </div>
                 )}
                 {anos.length > 0 && (
                     <div>
                         <label htmlFor="anos">Selecione o ano:</label>
-                        <select id="anos" name="anos" onChange={handleAnoChange}>
+                        <select id="anos" name="anos" onChange={handleAnoChange} value={anoSelecionado}>
                             <option value="">Selecione...</option>
-                            {anos.map(ano => (<option key={ano.codigo} value={ano.codigo}>{ano.nome}</option>
-                            ))}
+                            {anos.map(ano => (<option key={ano.codigo} value={ano.codigo}>{ano.nome}</option>))}
                         </select>
                     </div>
                 )}
             </div>
 
             <div className='formTable'>
-                {anoSelecionado.length > 0 && (
+                {anoSelecionado && details.Marca && (
                     <div>
                         <h2>Detalhes</h2>
                         <table>
@@ -126,8 +129,6 @@ function MarcasDropdown() {
                     </div>
                 )}
             </div>
-
-
         </div>
     );
 }
